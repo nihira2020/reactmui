4d971e79c38e9b2f59fa1f58defe6614d3ed4a54
Natarajan <nataraj.itsolution@gmail.com>
2023-05-13 12:25:31 +0530

    installation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { TextField,Stack,Box,Button } from '@mui/material';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <br>
+      </br>
+      {/* <TextField error disabled defaultValue="Nihira Techiees" variant='outlined' label="User Name"></TextField> */}
+      {/* <TextField variant='filled' label="Password"></TextField>
+      <TextField variant='standard' label="User Name"></TextField> */}
+      <Box component="form" sx={{
+        'margin':'1%',
+        'border':'2px solid #ccc',
+        // '.MuiInputBase-root':{'width':'130ch'},
+        // '.MuiButtonBase-root':{'width':'130ch'}
+      }}>
+<Stack direction="column" spacing={2} margin={2} >
+      <TextField label="User Name"></TextField>
+       <TextField type='password' label="Password"></TextField>
+       <TextField type='tel' label="Mobile"></TextField>
+        <TextField type='email' label="Email"></TextField>
+         <TextField multiline maxRows={3} minRows={2} label="Address"></TextField>
+          <TextField type='date' label="Date of Birth"></TextField>
+         <Button variant="contained">Submit</Button>
+         <Button color="secondary" variant="contained">Text</Button>
+         <Button color="success" variant="contained">Text</Button>
+          <Button color="error" variant="contained">Text</Button>
+      </Stack>
+      </Box>
     </div>
   );
 }
